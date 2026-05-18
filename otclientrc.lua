e3cf1399894bb69b6cd9847ecd6801d58a4b5af6
gchioka <gchioka@gmail.com>
2026-05-17 22:01:33 -0300

    fix: use port 80 via Nginx proxy for login server

--- otclientrc.lua
+++ otclientrc.lua
@@ -1,7 +1,7 @@
 -- DGOT OTClient
 
 Servers_init = {
-    ["http://2.24.72.76:8080/login.php"] = {
+    ["http://2.24.72.76/login.php"] = {
         port = 7172,
         protocol = 1500,
         httpLogin = true,
@@ -9,9 +9,9 @@ Servers_init = {
     }
 }
 
-g_settings.set("host", "http://2.24.72.76:8080/login.php")
+g_settings.set("host", "http://2.24.72.76/login.php")
 g_settings.set("port", 7172)
 g_settings.set("client-version", 1500)
 g_settings.set("httpLogin", true)
 
-print("DGOT carregado! Login: http://2.24.72.76:8080/login.php Game: 2.24.72.76:7172")
+print("DGOT carregado! Login: http://2.24.72.76/login.php Game: 2.24.72.76:7172")
